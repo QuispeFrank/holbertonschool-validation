245fbdfabf42e092cfe197a30b0233b802ceb7df
FranQ <frankchristopherquispe@gmail.com>
2023-01-06 15:33:11 -0500

    setup.sh <> test 10

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -3,6 +3,7 @@
 # 1
 apt-get update && apt-get install -y wget make
 apt-get install markdown -y
+npm install markdownlint --save-dev
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
 tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
 mv hugo /usr/local/bin/
