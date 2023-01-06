ae08537204cd81aa6f61a3aa98437775a52fcbb6
FranQ <frankchristopherquispe@gmail.com>
2023-01-06 16:05:18 -0500

    setup.sh, module3_task2.yml <> test 12

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -3,7 +3,7 @@
 # 1
 apt-get update && apt-get install -y wget make
 apt-get install markdown -y
-apt install npm
+apt install npm -y 
 npm install markdownlint --save-dev
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
 tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
